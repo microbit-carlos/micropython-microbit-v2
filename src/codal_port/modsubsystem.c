af9cd9113cefd2928be7c00e0cd4b6a6cd8a51fa
Carlos Pereira Atencio <carlos@microbit.org>
2024-03-08 18:50:35 +0000

    subsystem.info() to read 'hello.txt' file from filesystem.

--- src/codal_port/modsubsystem.c
+++ src/codal_port/modsubsystem.c
@@ -3,12 +3,41 @@
 
 #include "py/builtin.h"
 #include "py/runtime.h"
+#include "py/stream.h"
+#include "py/objstr.h"
+
 
 #if MICROPY_PY_SUBSYSTEM
 
 // info()
 STATIC mp_obj_t py_subsystem_info(void) {
-    return MP_OBJ_NEW_SMALL_INT(42);
+    mp_obj_t open_args[2] = {
+        mp_obj_new_str("hello.txt", strlen("hello.txt")), // filename
+        MP_OBJ_NEW_QSTR(MP_QSTR_rb)                       // read binary
+    };
+    // Obtaining the file descriptor
+    mp_obj_t hello_f_descriptor = mp_builtin_open(2, open_args, (mp_map_t *)&mp_const_empty_map);
+
+    // Creating a variable string to contain the file data (even if its binary)
+    size_t sz = 1024;
+    vstr_t vstr;
+    vstr_init(&vstr, sz);
+
+    // Reading the file with the stream module
+    int error;
+    mp_uint_t out_sz = mp_stream_read_exactly(hello_f_descriptor, vstr.buf, sz, &error);
+    vstr.len = out_sz;
+
+    // Check for errors reading the file
+    if (error != 0) {
+        if (mp_is_nonblocking_error(error)) {
+            return mp_const_none;
+        }
+        mp_raise_OSError(error);
+    } else {
+        // Return the content as a Python bytes object
+        return mp_obj_new_bytes_from_vstr(&vstr);
+    }
 }
 MP_DEFINE_CONST_FUN_OBJ_0(subsystem_info_obj, py_subsystem_info);
 
